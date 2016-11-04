6a67dd83af356aeeaee438966619c7f5f72b16cd
Khanh Duc Tran <ktran035@uw.edu>
2016-11-03 21:33:22 -0700

    added new functionality, which will populate data to ham and spam map

--- NaiveBayes.java
+++ NaiveBayes.java
@@ -4,8 +4,8 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class NaiveBayes {
-	public static HashMap<String, Double> spam_count;
-	public static HashMap<String, Double> ham_count;
+	public static HashMap<String, Double> spam_count = new HashMap<String, Double>();
+	public static HashMap<String, Double> ham_count = new HashMap<String, Double>();
 	//	This function reads in a file and returns a 
 	//	set of all the tokens. It ignores the subject line
 	//
@@ -41,19 +41,45 @@ public class NaiveBayes {
 	
 	public static void processFiles(File f_ham, File f_spam) throws IOException {
 		// For some reason .DS_store is included in list of files path
-		parseDataFromFile(f_ham.listFiles());
-		parseDataFromFile(f_spam.listFiles());
+		parseDataFromFile(f_ham.listFiles(), true);
+		parseDataFromFile(f_spam.listFiles(), false);
 	}
 	
-	public static void parseDataFromFile(File[] files) throws IOException {
+	public static void parseDataFromFile(File[] files, boolean ham_huh) throws IOException {
 		//HashMap<String, Double> result = new HashMap<String, Double>();
 		// process each text file
 		for (int i = 1; i < files.length; i++) {
 			//File curr_txt = new File(files[i]);
 			// set of all word in files
 			HashSet<String> word_in_file = tokenSet(files[i]);
+			parseCountData(word_in_file, ham_huh);
 			
 			System.out.println(word_in_file);
+		}
+	}
+	
+	// populate data for two ham and spam hash table
+	// where key is word, value is number of file contain that word
+	public static void parseCountData(HashSet<String> word_in_file, boolean ham_huh) {
+		for (String word: word_in_file) {
+			if (ham_huh) { // this file is a ham
+				populateDataCount(ham_count, word);
+			} else { // this email file is not a ham, it's spam
+				populateDataCount(spam_count, word);
+			}
+		}
+	}
+	
+	/*
+	 * @count_map it's either a spam or ham map
+	 * @word it's a current word that will be populated into either spam or ham
+	 */
+	public static void populateDataCount(HashMap<String, Double> count_map, String word) {
+		// check if the map contain the word already
+		if (count_map.containsKey(word)) {
+			count_map.put(word, count_map.get(word) + 1);
+		} else { // new word
+			count_map.put(word, 1.0);
 		}
 	}
 }
